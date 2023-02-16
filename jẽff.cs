1af194331355bc3ed2ee97fb16e54b613fa75718
AutisticRobot <tmkormann04@gmail.com>
2023-02-16 14:09:40 -0800

    started work on movement code

---
+++ jẽff.cs
@@ -0,0 +1,33 @@
+using Godot;
+using System;
+
+public class jẽff : AnimatedSprite
+{
+
+[Export]
+public int speed;
+
+[Export]
+public bool paused = false;
+
+
+public AnimatedSprite jẽff;
+public Vector2 screen;
+
+public override void _Ready()
+{
+    
+}
+
+
+public override void _Process(float delta)
+{
+    if(!paused)
+    {
+        //move code
+        Vector2 mov = new Vector2(0,0);
+    }
+}
+
+
+}
