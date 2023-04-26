537ca8ecbe8a8e501d4ba242be32964565e1a0fa
AutisticRobot <80130251+AutisticRobot@users.noreply.github.com>
2023-04-25 21:08:05 -0700

    fleshed out ai and movet it to its own file

--- Scripts/Lion.cs
+++ Scripts/Lion.cs
@@ -3,14 +3,17 @@ using System;
 
 public class Lion : Sprite
 {
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
-
+    [Export]
+    public float range = 100;
+    [Export]
+    public int speed = 10;
+    
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        
+        Vector2 validPos;
+        //validPos = ai.findValidPos(Position, range);//fix this
+        GD.Print("test");
     }
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -18,12 +21,5 @@ public class Lion : Sprite
 //  {
 //      
 //  }
-private void ai(float range)
-{
-    Vector2 target = new Vector2(0,0);
-    target.x = (float)GD.RandRange(-1,1);
-    target.y = (float)GD.RandRange(-1,1);
-    float distance = (float)GD.RandRange(0,range);
-    target * distance;
-}
+
 }
---
+++ Scripts/ai.cs
@@ -0,0 +1,35 @@
+using Godot;
+using System;
+
+public class ai : Node
+{
+
+public Vector2 findValidPos(Vector2 pos, float range)
+{
+    Vector2 tar;
+    float dis;
+    bool isValid = false;
+    int maxLoop = 5;
+    
+    do
+    {
+        
+        tar = new Vector2(0,0);
+        dis = (float)GD.RandRange(0,range);
+        tar.x = (float)GD.RandRange(-1,1);
+        tar.y = (float)GD.RandRange(-1,1);
+        tar *= dis;
+        tar += pos;
+        isValid = ( 0 <= tar.x && tar.x <= 1920 );
+        isValid = (isValid && 0 <= tar.y && tar.y <= 1080);
+        maxLoop--;
+    } while (!isValid && maxLoop > 0);
+
+    if(isValid)
+    {
+        return tar;
+    }else{
+        return pos;
+    }
+}
+}
